c020ff31ba4c8b403edd2a0cd1333cf8bd503f50
Justin Louie <22231868+JustinMYeung@users.noreply.github.com>
2023-04-21 12:00:47 +0800

    Create hetero poscar file
    Create new struc with coordinates from material 1 and 2
    Compile into single poscar file

--- combine.py
+++ combine.py
@@ -26,7 +26,7 @@ struc_1 * lattice2 * rotation
 struc_2 * lattice1
 """
 
-transformations = latticeTransformations(l1_2D, l2_2D, 100, 0.01)
+transformations = latticeTransformations(l1_2D, l2_2D, 20, 0.01)
 
 m = transformations[0]
 n = transformations[1]
@@ -40,40 +40,49 @@ n_3D = [[n[0][0], n[0][1], 0],
 		 [n[1][0], n[1][1], 0], 
 		  [0, 0, 1]]
 
-struc_1.make_supercell(scaling_matrix=n_3D)
-struc_1 = RotationTransformation([0,0,1], r, False).apply_transformation(struc_1)
+struc_1.make_supercell(scaling_matrix=m_3D)
+RotationTransformation([0,0,1], r, False).apply_transformation(struc_1)
 
-struc_2.make_supercell(scaling_matrix=m_3D)
+struc_2.make_supercell(scaling_matrix=n_3D)
+
+l1 = struc_1.lattice.matrix
+l2 = struc_2.lattice.matrix
+
+new_matrix = [[l2[0,0], l2[0,1], 0],
+	[l2[1,0], l2[1,1], 0],
+	[0, 0, 25]]
 
 struc_1_coords = struc_1.cart_coords
 struc_2_coords = struc_2.cart_coords
 
-"""
-Repositions struc_2 directly above struc_1
+struc_1_species = [site.species for site in struc_1.sites]
+struc_2_species = [site.species for site in struc_2.sites]
 
+# Positions struc 1 values to be directly below z = 0
+struc_1_coords[:,2] -= struc_1_coords[:,2].max()
 
-Finds the highest Z in struc_1
-Finds the lowest Z in struc_2
-Moves struc_2 to be arg[v] above struc_1
-"""
+# Positions struc 2 values to be directly at z = argv[3]
+struc_2_coords[:,2] -= (struc_2_coords[:,2].min() - float(argv[3]))
 
-maxZ1 = 0
-minZ2 = 1000
+hetero_coords = np.append(struc_1_coords, struc_2_coords, axis=0)
+# print(hetero_coords.shape)
+# print(struc_1_species + struc_2_species)
+# print(hetero_coords)
 
-for particle in struc_1_coords:
-	if (particle[2] >= maxZ1):
-		maxZ1 = particle[2]
+new_struc = Structure(new_matrix, species=struc_1_species + struc_2_species,
+	coords= hetero_coords, coords_are_cartesian=True)
 
-for particle in struc_2_coords:
-	if (particle[2] <= minZ2):
-		minZ2 = particle[2]
+c_vec = new_struc.lattice.matrix[2]
 
-zAdjust = maxZ1 - minZ2 + float(argv[3])
+struc_zs = new_struc.cart_coords[:, 2]
+struc_dist = struc_zs.max() - struc_zs.min()
+prev_centre = struc_zs.min() + (struc_dist / 2)
 
-for particle in struc_2_coords:
-	particle[2] += zAdjust 
+translation = c_vec / 2 - np.array([0, 0, prev_centre])
 
-# struc_1.to(filename='POSCAR_{}'.format('superlattice'))
+new_struc.translate_sites(np.arange(new_struc.cart_coords.shape[0]), vector=translation, frac_coords=False, to_unit_cell=True)
+
+new_struc.get_sorted_structure().to(filename='POSCAR_hetero')
 
 
 
